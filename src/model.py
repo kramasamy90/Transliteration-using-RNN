435e9650621166cec0bb9cb5c6133bb31c7456fb
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-05-13 15:07:53 +0530

    Implement Decoder.

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 sys.path.append(os.path.join(sys.path[0], '../src'))
@@ -27,8 +28,6 @@ class EncoderRNN(nn.Module):
     
     def forward(self, input):
         embedded = self.embedding(torch.where(input == 1)[0])
-        print(embedded.dtype)
-        print(self.hidden.dtype)
         output, hidden = self.RNN(embedded, self.hidden)
         return output, hidden
 
@@ -38,3 +37,37 @@ class EncoderRNN(nn.Module):
         else:
             self.hidden = torch.zeros(self.num_layers * self.hidden_size).view(self.num_layers, self.hidden_size)
 
+class DecoderRNN(nn.Module):
+    def __init__(self, embedding_size = 16, hidden_size = 16, num_layers = 1, cell_type = 'rnn', bidir = False, dropout = 0):
+        super(DecoderRNN, self).__init__()
+
+        self.input_size = 128
+        self.output_size = 128
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.cell_type = cell_type
+        self.bidir = bidir
+        self.dropout = dropout
+
+        self.embedding = nn.Embedding(self.input_size, self.embedding_size)
+        self.RNN = nn.RNN(input_size=self.embedding_size, hidden_size=self.hidden_size, num_layers=self.num_layers, bidirectional = self.bidir, dropout = self.dropout)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+        self.out_act = nn.LogSoftmax()
+        self.initialize_hidden()
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(torch.where(input == 1)[0])
+        embedded = F.relu(embedded)
+        output, hidden = self.RNN(embedded, self.hidden)
+        output = self.out(output)
+        print(output.shape)
+        print(hidden.shape)
+        output = self.out_act(output)
+        return output, hidden
+    
+    def initialize_hidden(self):
+        if(self.bidir):
+            self.hidden = torch.zeros(2 * self.num_layers * self.hidden_size).view(2 * self.num_layers, self.hidden_size)
+        else:
+            self.hidden = torch.zeros(self.num_layers * self.hidden_size).view(self.num_layers, self.hidden_size)
